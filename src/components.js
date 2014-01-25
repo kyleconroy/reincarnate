1c7dd1e8aaa20454a313f9764c3948285ca9a584
Sofiya Taskova <sofiya.taskova@moovweb.com>
2014-01-24 23:33:37 -0800

    Added some rocks (bumping into solid objects doesn't work yet) and a surface that happens to be on the right side. When worm touches surface, surface disappears

--- src/components.js
+++ src/components.js
@@ -6,8 +6,51 @@ Crafty.c('Actor', {
 
 Crafty.c('Worm', {
   init: function() {
-    this.requires('Actor, Fourway, Color')
+    this.requires('Actor, Fourway, Color, Collision')
       .fourway(4)
-      .color('black');
+      .color('rgb(255, 109, 188)')
+      .stopOnSolids()
+      .stopOnSolids()
+      // Whenever the PC touches a village, respond to the event
+      .onHit('Surface', this.visitSurface);
+  },
+ 
+  // Registers a stop-movement function to be called when
+  //  this entity hits an entity with the "Solid" component
+  stopOnSolids: function() {
+    this.onHit('Solid', this.stopMovement);
+ 
+    return this;
+  },
+ 
+  // Stops the movement
+  stopMovement: function() {
+    this._speed = 0;
+    if (this._movement) {
+      this.x -= this._movement.x;
+      this.y -= this._movement.y;
+    }
+  },
+  visitSurface: function(data) {
+    villlage = data[0].obj;
+    villlage.collect();
+  }
+});
+
+Crafty.c('Rock', {
+  init: function() {
+    this.requires('Actor, Color, Solid')
+      .color('rgb(89,89,89)');
+  },
+});
+Crafty.c('Surface', {
+  init: function() {
+    this.requires('Actor, Color')
+      .color('rgb(0, 125, 250)');
+  },
+ 
+  collect: function() {
+    this.destroy();
+    Crafty.trigger('SurfaceVisited', this);
   }
 });
--- src/game.js
+++ src/game.js
@@ -2,20 +2,76 @@ var e;
 
 Game = {
   // Initialize and start our game
+   // This defines our grid's size and the size of each of its tiles
+   map_grid: {
+    width:  50,
+    height: 32,
+    tile: {
+      width:  16,
+      height: 16
+    }
+  },
+
+  // The total width of the game screen. Since our grid takes up the entire screen
+  //  this is just the width of a tile times the width of the grid
+  width: function() {
+    return this.map_grid.width * this.map_grid.tile.width;
+  },
+
+  // The total height of the game screen. Since our grid takes up the entire screen
+  //  this is just the height of a tile times the height of the grid
+  height: function() {
+    return this.map_grid.height * this.map_grid.tile.height;
+  },
   start: function() {
     // Start crafty and set a background color so that we can see it's working
-    Crafty.init();
-    Crafty.background('green');
+    Crafty.init(Game.width(), Game.height());
+    Crafty.background('rgb(94,21,1)');
+    // Crafty.scene('Game');
+    for (var x = 0; x < Game.map_grid.width; x++) {
+      for (var y = 0; y < Game.map_grid.height; y++) {
+        var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
+
+        if (at_edge) {
+          // Place a tree entity at the current tile
+          Crafty.e('2D, Canvas, Color')
+          .attr({
+            x: x * Game.map_grid.tile.width,
+            y: y * Game.map_grid.tile.height,
+            w: Game.map_grid.tile.width,
+            h: Game.map_grid.tile.height
+          })
+          .color('rgb(89,89,89)');
+        } else if (Math.random() < 0.06) {
+          // Place a larger  at the current tile
+          Crafty.e('2D, Canvas, Color')
+          .attr({
+            x: x * Game.map_grid.tile.width,
+            y: y * Game.map_grid.tile.height,
+            w: Game.map_grid.tile.width,
+            h: Game.map_grid.tile.height
+          })
+          .color('rgb(89,89,89)');
+        }
+      }
+    }
 
     e = Crafty.e('Worm')
-      .attr({
-        x: (Crafty.viewport.width / 2) - 5,
-        y: (Crafty.viewport.height / 2) - 5,
-        w: 10,
-        h: 10,
-      })
+    .attr({
+      x: (Crafty.viewport.width / 2) - 5,
+      y: (Crafty.viewport.height / 2) - 5,
+      w: 10,
+      h: 20,
+    })
 
-    //Crafty.viewport.follow(e, 0, 0);
+    Crafty.e('Surface')
+    .attr({
+      x: (Crafty.viewport.width - 20),
+      y: 0,
+      w: 20,
+      h: (Crafty.viewport.height),
+    })
+
   }
 
 }
