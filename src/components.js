df0abab7e16188697cbe6552aabf1debe7dc3953
Kyle Conroy <kyle@kyleconroy.com>
2014-01-25 17:12:29 -0800

    Add the updated worm sprite

--- src/components.js
+++ src/components.js
@@ -18,11 +18,23 @@ Crafty.c('Earth', {
 Crafty.c('Worm', {
   init: function() {
     this.requires('Actor, Fourway, Collision, SpriteAnimation, WormSprite')
-    .fourway(2)
-    .reel('WormWriggle', 700, [[0, 0], [0, 1], [0, 2], [0, 1]])
-    .animate('WormWriggle', -1)
-    .onHit('Surface', this.visitSurface)
-    .onHit('Solid', this.stopMovement);
+      .fourway(2)
+      .reel('WormWriggle', 700, [[0,1], [0,0], [0,1], [0,2]])
+      .reel('WormMoveLeft', 700, [[0,1], [0,3], [0,4], [0,3]])
+      .reel('WormMoveRight', 700, [[0,1], [0,5], [0,6], [0,5]])
+      .animate('WormWriggle', -1)
+      .onHit('Surface', this.visitSurface)
+      .onHit('Solid', this.stopMovement);
+
+	  this.bind('NewDirection', function(data) {
+      if (data.x > 0) {
+        this.animate('WormMoveRight', -1);
+      } else if (data.x < 0) {
+        this.animate('WormMoveLeft', -1);
+      } else {
+        this.animate('WormWriggle', -1);
+      }
+    });
   },
 
   // Stops the movement
